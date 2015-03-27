86d7612aae70c2f4a446a9fbe33c4663acc52b71
ChrisMcKeownUK <Chris.McKeown@bedegaming.com>
2015-03-27 17:07:42 +0000

    Converted allowed groups to array

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,15 @@
 class npsradius (
   $policyname       = $npsradius::params::policyname,
   $allowedgroups    = $npsradius::params::allowedgroups,
-  $clientname       = $npsradius::params::clientname,
-  $clientip         = $npsradius::params::clientip,
-  $clientsecret     = $npsradius::params::clientsecret,
+  $clients          = $npsradius::params::clients,
 ) inherits npsradius::params {
 
   if !$allowedgroups {
     fail("No allowed groups were specified")
   }
 
-  if !$clientname {
-    fail("No RADIUS client name was supplied")
+  if !$clients {
+    fail("No RADIUS clients were supplied")
   }
 
   file { "C:/Windows/Temp/npsradius.xml":
